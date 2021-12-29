e1025221b54ece985107720a4ffcf62798eb80cd
Basudev <murabasudev@gmail.com>
2021-12-29 17:41:33 +0530

    Written some clean code for Mongo Uri and used dotenv

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
+require('dotenv').config();
 const port = 3000;
 
 //express middleware
@@ -17,4 +17,16 @@ app.use('/api/v1/tasks', tasks);
 
 app.get('/', (req, res) => res.send('Task Manager App'));
 
-app.listen(port, console.log(`Server is listening on ${port}`))
+
+const start = async () => {
+try {
+   await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on ${port}`))
+    
+} catch (error) {
+    console.log(error);
+}
+}
+
+start()
+
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose');
-const connectionString = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-console.log('Connected to MongoDB');
-}).catch((err) => { console.log(err);})
+const mongoose = require("mongoose");
+
+const connectDB = (url)=>{
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+}
+
+module.exports = connectDB;
